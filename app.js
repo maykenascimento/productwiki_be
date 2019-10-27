7bd64a4cb28f1f051a3e63b158e7fe8436a9910f
Zeppl Studios <mayke.eng@outlook.com>
2019-10-27 20:29:37 +0000

    Removed unnecessary files

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -19,26 +18,14 @@ var api = require('./routes/api');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
 app.use(cors());
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
 app.use(passport.initialize());
-
-// app.get('/', function (req, res) {
-//   // res.send('Page under construction.');
-//   res.render('index', { title: 'Home' });
-// });
 
 app.use('/api', api);
 
